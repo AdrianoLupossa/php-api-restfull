2a0169a4f2bfbde2e06474205c1c246832ab9a8b
adrianolupossa <adrianolupossa@gmail.com>
2019-01-14 17:12:46 +0100

    update errors handling

--- model/config.php
+++ model/config.php
@@ -9,7 +9,11 @@ function conectar () {
 	try {
 		$con = new PDO("mysql:host=".SERVER."; dbname=".DB, USER, PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"));
 		include("offline-db.php");
-		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$error = $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		if ($error) {
+			http_response_code(500);
+		}
+
 	} catch (PDOException $e) {
 		echo $e->getMessage();
 	}
--- model/offline-db.php
+++ model/offline-db.php
@@ -1,6 +1,6 @@
 <?php
 
-$query_stock = $con->query("create table `produtos`(codigo_produto int(255) not null primary key auto_increment, nome varchar(40) unique, preco varchar(6), responsavel varchar(100), codigo_barras varchar(15), quantidade_stock int(6), quantidade_entrada varchar(6), dataFab varchar(60), dataExp varchar(60), data_entrada varchar(60), editado tinyint)");
+$query_stock = $con->query("create table `produtos`(codigo_produto int(255) not null primary key auto_increment, nome varchar(40) unique, preco_compra varchar(6), preco_venda varchar(6), responsavel varchar(100), codigo_barras varchar(15), quantidade_stock int(6), quantidade_entrada varchar(6), dataFab varchar(60), dataExp varchar(60), data_entrada varchar(60), editado tinyint)");
 
 $query_empresa = $con->query("create table `empresa`(codigo_empresa int(255) not null primary key auto_increment, token varchar(100), FarmID varchar(60), nome varchar(40), telefone varchar(20), telefone2 varchar(20), email varchar(60), website varchar(100), endereco varchar(80), fornecedor varchar(4), clientes varchar(4), funcionarios varchar(4), data_registo varchar(100), editado tinyint, sincronizado tinyint)");
 
@@ -8,7 +8,7 @@ $query_login = $con->query("create table `login`(codigo_login int(255) not null 
 
 $query_funcionarios = $con->query("create table `funcionarios`(codigo_funcionario int not null primary key auto_increment, nome varchar(60), telefone varchar(20), morada varchar(100), bi varchar(40) unique, data_nascimento varchar(60), data_registo varchar(60), responsavel varchar(100), editado tinyint)");
 
-$query_fornecedores = $con->query("create table `fornecedores`(codigo_fornecedor int not null primary key auto_increment, nome varchar(60), empresa varchar(100), morada varchar(100), telefone varchar(40), nif varchar(30), email varchar(60), data_registo varchar(60), responsavel varchar(100), editado tyint)");
+$query_fornecedores = $con->query("create table `fornecedores`(codigo_fornecedor int not null primary key auto_increment, nome varchar(60), empresa varchar(100), morada varchar(100), telefone varchar(40), nif varchar(30), email varchar(60), data_registo varchar(60), responsavel varchar(100), editado tinyint)");
 
 $query_vendas = $con->query("create table `vendas`(codigo_venda int not null primary key auto_increment, codigo_produto int, quantidade int, total int, codigo_factura varchar(6), responsavel varchar(40), data_venda varchar(60), editado tinyint)");
 
